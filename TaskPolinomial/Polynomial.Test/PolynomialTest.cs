0e6738f435de99d95bb09bcb0490a207483b88e4
Stas Strezh <37235564+StasStrezh@users.noreply.github.com>
2018-03-25 15:21:05 +0300

    Add files via upload

---
+++ TaskPolinomial/Polynomial.Test/PolynomialTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using NUnit.Framework;
+using TaskPolinomial;
+
+namespace TaskPolinomial
+{
+    [TestFixture]
+    public class PolynomialTest
+    {
+        [TestCase(new double[] { 0, 0, 0, 0, 0 }, ExpectedResult = 0)]
+        public static int OrderProperty_PositiveTest(double[] array)
+        {
+            Polynomial p = new Polynomial(array);
+            return p.Order;
+        }
+
+        [TestCase(new double[] { 1, 4, 3, 7, 5 }, 3, ExpectedResult = 7)]
+        [TestCase(new double[] { 1, 4, 3, 7, 5 }, 4, ExpectedResult = 5)]
+        [TestCase(new double[] { 1, 4, 3, 7, 5 }, 5, ExpectedResult = 0)]
+        public static double CoeffProperty_PositiveTest(double[] array, int factor)
+        {
+            Polynomial p = new Polynomial(array);
+            return p[factor];
+        }
+
+        [TestCase(new double[] { 1, 4, 3 }, new double[] { 0, 0, 0, 1, 4, 3 })]
+        public static void PlusOperation_PositiveTest(double[] array1, double[] array2)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Polynomial p = p1 + p2;
+
+            double[] expected = new double[p2.Order + 1];
+            for (int i = 0; i < array1.Length; i++)
+                expected[i] = array1[i] + array2[i];
+            for (int i = array1.Length; i < array2.Length; i++)
+                expected[i] = array2[i];
+
+            CollectionAssert.AreEqual(expected, p.Index);
+        }
+
+        [TestCase(new double[] { 1, 4, 3 }, new double[] { 0, 0, 0, 1, 4, 3 })]
+        public static void MinusOperation_PositiveTest(double[] array1, double[] array2)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Polynomial p = p1 - p2;
+
+            double[] expected = new double[p2.Order + 1];
+            for (int i = 0; i < array1.Length; i++)
+                expected[i] = array1[i] - array2[i];
+            for (int i = array1.Length; i < array2.Length; i++)
+                expected[i] = -array2[i];
+
+            CollectionAssert.AreEqual(expected, p.Index);
+        }
+
+        [TestCase(new double[] { 1, 4 }, new double[] { 1, 8, 16 })]
+        public static void MultiplyByPolynomialOperation_PositiveTest(double[] array, double[] expected)
+        {
+            Polynomial p = new Polynomial(array);
+            p = p * p;
+
+            CollectionAssert.AreEqual(expected, p.Index);
+        }
+
+        [TestCase(new double[] { 1, 2 }, ExpectedResult = "1 + 2*x^1")]
+        [TestCase(new double[] { 1, 0, 2 }, ExpectedResult = "1 + 2*x^2")]
+        [TestCase(new double[] { 0, 1, 2 }, ExpectedResult = "1*x^1 + 2*x^2")]
+        public static string ToString_PositiveTest(double[] array)
+        {
+            Polynomial p = new Polynomial(array);
+            return p.ToString();
+        }
+
+    }
+}
---
+++ TaskPolinomial/TaskPolinomial/Polynomial.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPolinomial
+{
+    public sealed class Polynomial
+    {
+        private const double eps = double.Epsilon;
+
+        public Polynomial(double[] index)
+        {
+            this.Index = index;
+        }
+
+        public int Order
+        {
+            get
+            {
+                for (int i = Index.Length - 1; i >= 0; i--)
+                    if (Math.Abs(Index[i]) > eps)
+                        return 1;
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Property for index
+        /// </summary>
+        public double[] Index { get; private set; }
+        /// <summary>
+        /// Indexator
+        /// </summary>
+        /// <param name="factor"></param>
+        /// <returns></returns>
+        public double this[int factor]
+        {
+            get
+            {
+                if (factor < 0) throw new ArgumentOutOfRangeException();
+                if (factor >= Index.Length) return 0;
+                return Index[factor];
+            }
+        }
+
+        /// <summary>
+        /// Overload +
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public static Polynomial operator +(Polynomial first, Polynomial second)
+        {
+            int maxLength = 1 + (first.Order > second.Order ? first.Order : second.Order);
+            int minLength = 1 + (first.Order < second.Order ? first.Order : second.Order);
+
+            double[] resultFactors = new double[maxLength];
+            for (int i = 0; i < minLength; i++)
+                resultFactors[i] = first[i] + second[i];
+
+            if (first.Order > second.Order)
+                Array.Copy(first.Index, minLength, resultFactors, minLength, maxLength - minLength);
+            else
+                Array.Copy(second.Index, minLength, resultFactors, minLength, maxLength - minLength);
+
+            return new Polynomial(resultFactors);
+        }
+
+        /// <summary>
+        /// Overload -
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public static Polynomial operator -(Polynomial first, Polynomial second)
+        {
+            int maxLength = 1 + (first.Order > second.Order ? first.Order : second.Order);
+            int minLength = 1 + (first.Order < second.Order ? first.Order : second.Order);
+
+            double[] resultFactors = new double[maxLength];
+            for (int i = 0; i < minLength; i++)
+                resultFactors[i] = first[i] - second[i];
+
+            if (first.Order > second.Order)
+                Array.Copy(first.Index, minLength, resultFactors, minLength, maxLength - minLength);
+            else
+                for (int i = minLength; i < maxLength; i++)
+                    resultFactors[i] = -second[i];
+
+            return new Polynomial(resultFactors);
+        }
+
+        /// <summary>
+        /// Overloads *
+        /// </summary>
+        /// <param name="polinomial"></param>
+        /// <param name="multiplier"></param>
+        /// <returns></returns>
+        public static Polynomial operator *(Polynomial polinomial, int multiplier)
+        {
+            double[] resultFactors = new double[polinomial.Order + 1];
+            for (int i = 0; i < polinomial.Order + 1; i++)
+                resultFactors[i] = polinomial[i] * multiplier;
+            return new Polynomial(resultFactors);
+        }
+
+        public static Polynomial operator *(int multiplier, Polynomial polinomial)
+        {
+            return polinomial * multiplier;
+        }
+
+        public static Polynomial operator *(Polynomial polinomial, double multiplier)
+        {
+            double[] resultFactors = new double[polinomial.Order + 1];
+            for (int i = 0; i < polinomial.Order + 1; i++)
+                resultFactors[i] = polinomial[i] * multiplier;
+            return new Polynomial(resultFactors);
+        }
+
+        public static Polynomial operator *(double multiplier, Polynomial polinomial)
+        {
+            return polinomial * multiplier;
+        }
+
+        public static Polynomial operator *(Polynomial first, Polynomial second)
+        {
+            int resultOrder = first.Order + second.Order + 1;
+            double[] resultForces = new double[resultOrder];
+
+            for (int i = 0; i <= first.Order; i++)
+            {
+                if (Math.Abs(first[i]) > eps)
+                {
+                    for (int j = 0; j <= second.Order; j++)
+                        resultForces[i + j] += first[i] * second[j];
+                }
+            }
+
+            return new Polynomial(resultForces);
+        }
+
+        /// <summary>
+        /// Overloads /
+        /// </summary>
+        /// <param name="polinomial"></param>
+        /// <param name="multiplier"></param>
+        /// <returns></returns>
+        public static Polynomial operator /(Polynomial polinomial, int multiplier)
+        {
+            double[] resultFactors = new double[polinomial.Order + 1];
+            for (int i = 0; i < polinomial.Order + 1; i++)
+                resultFactors[i] = polinomial[i] / multiplier;
+            return new Polynomial(resultFactors);
+        }
+
+        public static Polynomial operator /(int multiplier, Polynomial polinomial)
+        {
+            return polinomial / multiplier;
+        }
+
+        public static bool operator ==(Polynomial first, Polynomial second)
+        {
+            if (first.Index.Length != second.Index.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < first.Index.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool operator !=(Polynomial first, Polynomial second)
+        {
+            return !(first == second);
+        }
+
+
+        /// <summary>
+        /// Provides string representation of polynomial
+        /// </summary>
+        /// <returns>String representation of polynomial</returns>
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < Index.Length; i++)
+            {
+                if (i == 0 && Math.Abs(Index[i]) > eps)
+                {
+                    str.AppendFormat($"{Index[i]}");
+                    continue;
+                }
+
+                if (Math.Abs(Index[i]) > eps)
+                    if (Index[i] > 0 && str.Length > 0)
+                        str.AppendFormat($" + {Index[i]}*x^{i}");
+                    else
+                        str.AppendFormat($" {Index[i]}*x^{i}");
+            }
+
+            return str.ToString().Trim();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Polynomial p = obj as Polynomial;
+
+            if (p?.Order != this.Order)
+                return false;
+
+            for (int i = 0; i <= this.Order; i++)
+            {
+                if (Math.Abs(this[i] - p[i]) > eps)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 1;
+            foreach (var factor in Index)
+            {
+                hash *= (int)factor;
+                hash += Order;
+            }
+
+            return hash;
+        }
+    }
+}
